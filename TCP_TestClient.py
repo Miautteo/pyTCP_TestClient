41cb1cac1924d6d2c81d0e00ff37ae1d9ed619b0
Matteo403510 <answeringel@icloud.com>
2022-11-19 00:55:22 +0100

    modified:   TCP_TestClient.py

--- TCP_TestClient.py
+++ TCP_TestClient.py
@@ -1,14 +1,13 @@
 import socket as S
 
 ##Establish variables:
-#Address = "192.168.178.58"
-Address = "2a02:908:f10:b440:d8fe:715f:f16:8ec9 "
+Address = "2a02:908:f10:b440:d8fe:715f:f16:8ec9"
 Port = 11000
 encoding = "utf-8"
 
 ##Establish Socket
-socket = S.socket(S.AF_INET6, S.SOCK_STREAM)
-socket.connect((Address, Port))
+socket = S.socket(S.AF_INET6, S.SOCK_STREAM, 0)
+socket.connect((Address, Port, 0, 0))
 
 ##Define Data
 Msg = "Hallo ü"
